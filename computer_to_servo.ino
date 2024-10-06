d6b180838b7b551ce3fa95f6c776f2a3b54f58c2
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2024-10-05 20:00:20 -0400

    very basic arduino communication has been implemented

---
+++ computer_to_servo.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+Servo Servo1;
+//
+int servoPin = 9;
+//int potPin = A0;
+
+int angle = 90;
+
+void setup() {
+  Serial.begin(9600);
+  
+  // put your setup code here, to run once:
+  Servo1.attach(servoPin);
+  Servo1.write(angle);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    if(Serial.available()>0)
+    {
+      String msg = Serial.readString();
+      angle = msg.toInt();
+
+      //Serial.println("100");
+      Servo1.write(angle);    
+    }
+    
+}
--- vision2.py
+++ vision2.py
@@ -1,3 +1,6 @@
+# import serial for access to arduino
+import serial.tools.list_ports
+
 #Import the following classes to access the Face Detector task functions:
 import mediapipe as mp
 from mediapipe.tasks import python
@@ -8,6 +11,14 @@ import utilities as util
 #basic cv2 stuff
 import cv2 
 
+#port set up
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
+
+serialInst.baudrate = 9600
+serialInst.port = ports[0].device
+serialInst.open()
+
 #ready media pipe
 BaseOptions = mp.tasks.BaseOptions
 FaceDetector = mp.tasks.vision.FaceDetector
@@ -15,11 +26,16 @@ FaceDetectorOptions = mp.tasks.vision.FaceDetectorOptions
 FaceDetectorResult = mp.tasks.vision.FaceDetectorResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
+# recent Data class is just a nice way of storing the most recent face detection
+# data since it runs asyncynously
 class RecentData:
   def __init__(self):
     self.results = None
     self.time = None
 
+  def top():
+    return holdRes, holdTime
+  
   def pop(self):
     if self.results:
       holdRes = self.results
@@ -36,9 +52,18 @@ class RecentData:
     self.results = result
     self.time = time
 
+
+# instantiate the data holder
 dataHold = RecentData()
 
+up = 0
+
 # Create a face detector instance with the live stream mode:
+# on the documentation, this was initally print results
+# i changed to process since i want this data back in the while loop
+# attempting to draw output inside this function resulted in terrible framerate
+# so having it in the while loop, grabbing what ever was in data hold
+# produced a much nicer outcome
 def process_result(result: FaceDetectorResult, output_image: mp.Image, timestamp_ms: int):
     #print('face detector result: {}'.format(result))
     if result.detections:
@@ -46,7 +71,7 @@ def process_result(result: FaceDetectorResult, output_image: mp.Image, timestamp
     #print(" ")
     
 
-
+#set for the face detection on google's documentations
 options = FaceDetectorOptions(
     base_options=BaseOptions(model_asset_path='Models/blaze_face_short_range.tflite'),
     running_mode=VisionRunningMode.LIVE_STREAM,
@@ -54,7 +79,8 @@ options = FaceDetectorOptions(
 with FaceDetector.create_from_options(options) as detector:
   # The detector is initialized. Use it here.
   # ...
-    
+  
+  #grab the cam to be used
   cap = cv2.VideoCapture(1)
 
   # Use OpenCV’s VideoCapture to start capturing from the webcam.
@@ -66,11 +92,12 @@ with FaceDetector.create_from_options(options) as detector:
     if not ret:
       continue
 
+    #quit program via "q" press
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
 
-    bbox = None
 
+    #grab center of screen for calculations
     centerScreenX = int(len(frame[0])/2)
     centerScreenY = int(len(frame)/2)
 
@@ -83,8 +110,6 @@ with FaceDetector.create_from_options(options) as detector:
     # the `FaceDetectorOptions` object.
     # The face detector must be created with the live stream mode.
     frame_timestamp_ms = int(cap.get(cv2.CAP_PROP_POS_MSEC))
-
-    
 
     detector.detect_async(mp_image, frame_timestamp_ms)
 
@@ -110,11 +135,24 @@ with FaceDetector.create_from_options(options) as detector:
 
       cv2.line(frame, (centerScreenX, centerScreenY), (centerRecX, centerRecY), (255, 255, 0), 3)
 
+      if centerRecY > centerScreenY:
+        if up != -1:
+          serialInst.write(str(180).encode('utf-8'))
+          up = -1
+      elif centerRecY < centerScreenY:
+        if up != 1:
+          serialInst.write(str(0).encode('utf-8'))
+          up = 1
+      #serialInst.write(str((centerRecY / len(frame)) * 180).encode('utf-8'))
 
       cv2.imshow('cam', frame)
     else:
       cv2.imshow('cam',frame)#cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
 
+  # release camera
   cap.release()
   cv2.destroyAllWindows()
+
+  #release port
+  serialInst.close()
